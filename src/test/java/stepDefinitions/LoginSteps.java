de455c5c1536d3d06a5d9756e5625dcefe22eb63
Adhiraj Singh <sfcadhiraj@gmail.com>
2021-02-11 23:30:37 +0530

    Updated Login Steps

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,4 +1,61 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
+
+
+
+    @Given("user is on Login page")
+    public void user_is_on_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user gets Login title of the page")
+    public void user_gets_login_title_of_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("page title should be {string}")
+    public void page_title_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters username {string}")
+    public void user_enters_username(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("user enters password {string}")
+    public void user_enters_password(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @When("user clicks on Login button")
+    public void user_clicks_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @Then("user gets Homepage title of the page")
+    public void user_gets_homepage_title_of_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
 }
--- src/test/resources/com.Features/LoginPage.feature
+++ src/test/resources/com.Features/LoginPage.feature
@@ -1,7 +1,7 @@
 Feature: Login Page Validation
 Scenario: Login Page Title
-  Given: user is on Login Page
-  When user gets title of the page
+  Given user is on Login Page
+  When user gets Login title of the page
   Then page title should be "Login | Salesforce"
 
   Scenario: Login with Correct Credentials
@@ -9,5 +9,5 @@ Scenario: Login Page Title
   When user enters username "adhsingh2@publicisgroupe.net"
   And user enters password "Feb2021@"
   And user clicks on Login button
-  Then user gets title of the page
+  Then user gets Homepage title of the page
   And page title should be "Lightning experience  | Salesforce"
